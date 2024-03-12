b7fe07858f0c8c24f7fc9b188b5d40b9c801b5c6
Mina Mikhail <mina@fightingtheboss.com>
2024-03-12 09:19:44 -0400

    Lint Rubocop issues across generated files

--- app/controllers/sessions/passwordlesses_controller.rb
+++ app/controllers/sessions/passwordlesses_controller.rb
@@ -1,32 +1,39 @@
-class Sessions::PasswordlessesController < ApplicationController
-  skip_before_action :authenticate
+# frozen_string_literal: true
 
-  before_action :set_user, only: :edit
+module Sessions
+  class PasswordlessesController < ApplicationController
+    skip_before_action :authenticate
 
-  def new
-  end
+    before_action :set_user, only: :edit
 
-  def edit
-    session_record = @user.sessions.create!
-    cookies.signed.permanent[:session_token] = { value: session_record.id, httponly: true }
-
-    revoke_tokens; redirect_to(root_path, notice: "Signed in successfully")
-  end
-
-  def create
-    if @user = User.find_by(email: params[:email], verified: true)
-      send_passwordless_email
-      redirect_to sign_in_path, notice: "Check your email for sign in instructions"
-    else
-      redirect_to new_sessions_passwordless_path, alert: "You can't sign in until you verify your email"
+    def new
     end
-  end
 
-  private
+    def edit
+      session_record = @user.sessions.create!
+      cookies.signed.permanent[:session_token] = { value: session_record.id, httponly: true }
+
+      revoke_tokens
+
+      redirect_to(root_path, notice: "Signed in successfully")
+    end
+
+    def create
+      if (@user = User.find_by(email: params[:email], verified: true))
+        send_passwordless_email
+        redirect_to(sign_in_path, notice: "Check your email for sign in instructions")
+      else
+        redirect_to(new_sessions_passwordless_path, alert: "You can't sign in until you verify your email")
+      end
+    end
+
+    private
+
     def set_user
-      token = SignInToken.find_signed!(params[:sid]); @user = token.user
+      token = SignInToken.find_signed!(params[:sid])
+      @user = token.user
     rescue StandardError
-      redirect_to new_sessions_passwordless_path, alert: "That sign in link is invalid"
+      redirect_to(new_sessions_passwordless_path, alert: "That sign in link is invalid")
     end
 
     def send_passwordless_email
@@ -36,4 +43,5 @@ class Sessions::PasswordlessesController < ApplicationController
     def revoke_tokens
       @user.sign_in_tokens.delete_all
     end
+  end
 end
