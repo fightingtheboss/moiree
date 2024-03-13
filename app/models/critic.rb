16fd1cff581d692f441fe1e4ba1a10677e0874ab
Mina Mikhail <mina@fightingtheboss.com>
2024-03-12 23:04:54 -0400

    Add country validation on Critic

--- app/models/critic.rb
+++ app/models/critic.rb
@@ -2,6 +2,7 @@ class Critic < ApplicationRecord
   include Userable
 
   validates :first_name, :last_name, presence: true
+  validates :country, inclusion: { in: ISO3166::Country.all.map(&:alpha2) }
 
   def name
     "#{first_name} #{last_name}"
