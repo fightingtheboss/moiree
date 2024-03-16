3091a3ae84d9746f895c299d2c7c5901a9e58620
Mina Mikhail <mina@fightingtheboss.com>
2024-03-16 12:06:02 -0400

    Flesh out tests for Rating

--- test/models/rating_test.rb
+++ test/models/rating_test.rb
@@ -4,30 +4,53 @@ require "test_helper"
 
 class RatingTest < ActiveSupport::TestCase
   test "should not be valid if score is below 0" do
+    rating = Rating.new(score: -1)
+
+    assert_not(rating.valid?)
   end
 
   test "should not be valid if score is above 5" do
+    rating = Rating.new(score: 6)
+
+    assert_not(rating.valid?)
   end
 
   test "should not be valid if score is formatted incorrectly" do
+    rating = Rating.new(score: 2.75)
+
+    assert_not(rating.valid?)
   end
 
   test "should not be valid if required fields are not present" do
+    rating = Rating.new
+
+    assert_not(rating.valid?)
   end
 
   test "should be valid with all required fields present" do
+    rating = ratings(:base)
+
+    assert(rating.valid?)
   end
 
   test "#film should return the Film associated with the Rating" do
     rating = ratings(:base)
     film = films(:base)
 
-    assert_equal film, rating.film
+    assert_equal(film, rating.film)
   end
 
-  test "should update Film#overall_average_rating when a new Rating is created" do
+  test "should call Film#overall_average_rating when a new Rating is created" do
+    rating = Rating.new(score: 5, critic: critics(:base), selection: selections(:base))
+    rating.film.expects(:cache_overall_average_rating)
+
+    rating.save
   end
 
-  test "should update Film#overall_average_rating when a new Rating is destroyed" do
+  test "should call Film#overall_average_rating when a Rating is destroyed" do
+    rating = ratings(:base)
+    rating.film.expects(:cache_overall_average_rating)
+
+    rating.destroy
   end
 end
