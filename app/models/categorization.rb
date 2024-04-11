c9b2ef70847d5fb8cc4bff5a2c9c2f38db4c1335
Mina Mikhail <mina@fightingtheboss.com>
2024-04-10 22:46:53 -0400

    Add edition association on Categorization

--- app/models/categorization.rb
+++ app/models/categorization.rb
@@ -4,6 +4,8 @@ class Categorization < ApplicationRecord
   belongs_to :category, inverse_of: :categorizations
   belongs_to :film, inverse_of: :categorizations
 
+  has_one :edition, through: :category
+
   validates :category_id, uniqueness: { scope: :film_id }
 
   accepts_nested_attributes_for :category
