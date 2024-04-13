4306466cc190604a63eee666ce2d07d94842c03a
Mina Mikhail <mina@fightingtheboss.com>
2024-04-13 17:37:56 -0400

    Update tests on FilmsController (and fixup films fixtures)

--- test/controllers/admin/films_controller_test.rb
+++ test/controllers/admin/films_controller_test.rb
@@ -1,8 +1,49 @@
+# frozen_string_literal: true
+
 require "test_helper"
 
 class Admin::FilmsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get admin_films_new_url
+  setup do
+    sign_in_as(users(:admin))
+  end
+
+  test "should get search with no params" do
+    get admin_films_search_url
+    assert_response :success
+  end
+
+  test "should get search with query" do
+    get admin_films_search_url, params: { query: "Title" }
+    assert_response :success
+  end
+
+  test "should get search with query and edition_id" do
+    edition = editions(:base)
+    get admin_films_search_url, params: { query: "Title", edition_id: edition.id }
+    assert_response :success
+  end
+
+  test "should get search with query, edition_id and exclude_films_in_edition" do
+    edition = editions(:base)
+    get admin_films_search_url, params: { query: "Title", edition_id: edition.id, exclude_films_in_edition: true }
+    assert_response :success
+  end
+
+  test "should get search with edition_id" do
+    edition = editions(:base)
+    get admin_films_search_url, params: { edition_id: edition.id }
+    assert_response :success
+  end
+
+  test "should get search with edition_id and exclude_films_in_edition" do
+    edition = editions(:base)
+    get admin_films_search_url, params: { edition_id: edition.id, exclude_films_in_edition: true }
+    assert_response :success
+  end
+
+  test "should get search_for_film_to_add_to_edition" do
+    edition = editions(:base)
+    get admin_festival_edition_search_for_film_to_add_url(edition.festival, edition), params: { query: "Title" }
     assert_response :success
   end
 end
