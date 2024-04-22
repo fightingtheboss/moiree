98b91e40a85eb68aa663a69866acf80555df6ec5
Mina Mikhail <mina@fightingtheboss.com>
2024-04-22 17:56:48 -0400

    Add Sendgrid credentials and config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,19 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: "moire.fly.dev", protocol: "https" }
+
+  if Rails.application.credentials.sendgrid
+    ActionMailer::Base.smtp_settings = {
+      user_name: Rails.application.credentials.sendgrid.fetch(:username),
+      password: Rails.application.credentials.sendgrid.fetch(:password),
+      address: "smtp.sendgrid.net",
+      port: 587,
+      authentication: :plain,
+      enable_starttls_auto: true,
+    }
+  end
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
