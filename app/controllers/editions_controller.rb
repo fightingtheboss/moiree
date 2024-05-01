b044a1c1e2bfaae9165652835348e63c1aa9cfed
Mina Mikhail <mina@fightingtheboss.com>
2024-04-30 21:09:45 -0400

    Add categories to editions#show

--- app/controllers/editions_controller.rb
+++ app/controllers/editions_controller.rb
@@ -7,7 +7,10 @@ class EditionsController < ApplicationController
 
   def show
     @edition = Edition.find(params[:id])
-    @selections = @edition.selections.includes(:film, ratings: :critic)
+    @selections = @edition.selections_with_categories.includes(ratings: :critic).order("films.title")
+    @selections_by_category = @selections.group_by do |selection|
+                                selection.film.categories.first
+                              end.sort_by { |category, _selections| category&.name }
     @critics = @selections.map(&:ratings).flatten.compact.map(&:critic).uniq.sort_by(&:last_name)
     @ratings = @critics.each_with_object({}) do |critic, selection_hash|
       selection_hash[critic] = @selections.each_with_object({}) do |selection, rating_hash|
