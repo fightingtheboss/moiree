1c16a623736cf3f59ca0b85af10bcf389b4c9961
Mina Mikhail <mina@fightingtheboss.com>
2024-05-16 01:43:51 -0400

    Show 🔥 and 💣 in editions grid

--- app/helpers/editions_helper.rb
+++ app/helpers/editions_helper.rb
@@ -1,2 +1,13 @@
+# frozen_string_literal: true
+
 module EditionsHelper
+  def display_rating(score)
+    if score.zero?
+      "💣"
+    elsif score == 5.0
+      "🔥"
+    else
+      score
+    end
+  end
 end
