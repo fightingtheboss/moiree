843943fdebf0ca926bb1edf463dd7e64c9a4252c
Mina Mikhail <mina@fightingtheboss.com>
2024-05-18 17:31:56 -0400

    Only use Bugsnag in production

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,4 +1,4 @@
-if Rails.application.credentials.bugsnag.present?
+if Rails.application.credentials.bugsnag.present? && Rails.env.production?
   Bugsnag.configure do |config|
     config.api_key = Rails.application.credentials.bugsnag.api_key
   end
