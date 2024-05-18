38ed4e1004d2e3b4c4951608d73d847a8f671dc3
Mina Mikhail <mina@fightingtheboss.com>
2024-05-18 10:58:57 -0400

    Don't initialize bugsnag on Docker build

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,3 +1,5 @@
-Bugsnag.configure do |config|
-  config.api_key = Rails.application.credentials.bugsnag.api_key
+if Rails.application.credentials.bugsnag.present?
+  Bugsnag.configure do |config|
+    config.api_key = Rails.application.credentials.bugsnag.api_key
+  end
 end
