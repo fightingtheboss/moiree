4a15d61f5ebdba47122be579d6900257de9b3d36
Mina Mikhail <mina@fightingtheboss.com>
2024-07-31 22:11:02 -0400

    Move attendances logic to Critic::Attendee concern

--- app/models/critic.rb
+++ app/models/critic.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 class Critic < ApplicationRecord
-  include Userable
+  include Userable, Attendee
   include FriendlyId
 
-  has_many :attendances, dependent: :destroy
   has_many :editions, through: :attendances
 
   has_many :ratings, dependent: :destroy do
@@ -17,8 +16,6 @@ class Critic < ApplicationRecord
 
   validates :first_name, :last_name, presence: true
   validates :country, inclusion: { in: ISO3166::Country.all.map(&:alpha2) }
-
-  accepts_nested_attributes_for :attendances
 
   friendly_id :slug_candidates, use: :slugged
 
---
+++ app/models/critic/attendee.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Critic::Attendee
+  extend ActiveSupport::Concern
+
+  included do
+    has_many :attendances, dependent: :destroy
+    accepts_nested_attributes_for :attendances
+  end
+
+  def publication_for(edition:)
+    @cached_publication ||= attendances.find_by(edition: edition)&.publication || publication
+  end
+
+  def has_custom_publication?(edition:)
+    publication != publication_for(edition: edition)
+  end
+end
