e9aaff98b38578d20f2d9dce5d73443041e473ad
Mina Mikhail <mina@fightingtheboss.com>
2024-07-31 22:16:12 -0400

    Add tests for new methods on Critic model

--- test/models/critic_test.rb
+++ test/models/critic_test.rb
@@ -35,4 +35,38 @@ class CriticTest < ActiveSupport::TestCase
 
     assert(critic.films.include?(film))
   end
+
+  test "#publication_for returns the Critic's base publication when no Attendance override set" do
+    critic = critics(:base)
+    edition = editions(:base)
+
+    assert(critic.publication_for(edition: edition) == critic.publication)
+  end
+
+  test "#publication_for returns the Critic's Attendance publication when set" do
+    critic = critics(:base)
+    edition = editions(:base)
+    attendance = attendances(:base)
+
+    attendance.update!(publication: "Override")
+
+    assert(critic.publication_for(edition: edition) == "Override")
+  end
+
+  test "#has_custom_publication? returns true when the Critic's publication is different from the Attendance publication" do
+    critic = critics(:base)
+    edition = editions(:base)
+    attendance = attendances(:base)
+
+    attendance.update!(publication: "Override")
+
+    assert(critic.has_custom_publication?(edition: edition))
+  end
+
+  test "#has_custom_publication? returns false when the Critic's publication is the same as the Attendance publication" do
+    critic = critics(:base)
+    edition = editions(:base)
+
+    assert_not(critic.has_custom_publication?(edition: edition))
+  end
 end
