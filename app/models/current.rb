de370faf2639b45e049e61e5fcfb29085146a334
Mina Mikhail <mina@fightingtheboss.com>
2024-08-20 14:04:09 -0400

    Allow admins to add ratings to past festivals

--- app/models/current.rb
+++ app/models/current.rb
@@ -1,6 +1,14 @@
+# frozen_string_literal: true
+
 class Current < ActiveSupport::CurrentAttributes
   attribute :session
   attribute :user_agent, :ip_address
 
   delegate :user, to: :session, allow_nil: true
+
+  class << self
+    def critic_for?(edition:)
+      user.critic? && user.userable.editions.include?(edition)
+    end
+  end
 end
