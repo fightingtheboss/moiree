689088a6c8f7f35f0f18fe28d6d18785e6fcd403
Mina Mikhail <mina@fightingtheboss.com>
2024-10-21 23:06:49 -0400

    Add scheduled job to upload a db backup to s3 daily

---
+++ app/jobs/backup_db_to_s3_job.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class BackupDbToS3Job < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    if Rails.env.production?
+      system("litefs export -name production.sqlite3 /tmp/latest.db")
+      system("gzip /tmp/latest.db")
+
+      role_credentials = Aws::AssumeRoleWebIdentityCredentials.new(
+        role_arn: ENV["AWS_ROLE_ARN"],
+        web_identity_token_file: ENV["AWS_WEB_IDENTITY_TOKEN_FILE"],
+        role_session_name: ENV["AWS_ROLE_SESSION_NAME"],
+      )
+
+      client = Aws::S3::Client.new(credentials: role_credentials, region: "us-east-1")
+
+      client.put_object(
+        bucket: "moiree",
+        body: File.open("/tmp/latest.db.gz"),
+        key: "moiree-production-#{Time.zone.now.strftime("%d%m%Y")}.db.gz",
+      )
+    end
+  end
+end
---
+++ test/jobs/backup_db_to_s3_job_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class BackupDbToS3JobTest < ActiveJob::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
