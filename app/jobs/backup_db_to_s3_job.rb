cc672bf5247bff4892526c35e3c1d621ce22c406
Mina Mikhail <mina@fightingtheboss.com>
2024-10-22 10:01:29 -0400

    Fix BackupDbToS3Job by using client on credentials object

--- app/jobs/backup_db_to_s3_job.rb
+++ app/jobs/backup_db_to_s3_job.rb
@@ -12,11 +12,10 @@ class BackupDbToS3Job < ApplicationJob
         role_arn: ENV["AWS_ROLE_ARN"],
         web_identity_token_file: ENV["AWS_WEB_IDENTITY_TOKEN_FILE"],
         role_session_name: ENV["AWS_ROLE_SESSION_NAME"],
+        region: "us-east-1",
       )
 
-      client = Aws::S3::Client.new(credentials: role_credentials, region: "us-east-1")
-
-      client.put_object(
+      role_credentials.client.put_object(
         bucket: "moiree",
         body: File.open("/tmp/latest.db.gz"),
         key: "moiree-production-#{Time.zone.now.strftime("%d%m%Y")}.db.gz",
