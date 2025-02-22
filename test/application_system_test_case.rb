992820967fa705058b19158e5d6990d227fd5b3a
Mina Mikhail <mina@fightingtheboss.com>
2025-02-22 21:34:56 +0000

    Update name of devcontainer and service

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,7 +1,18 @@
+# frozen_string_literal: true
+
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+  if ENV["CAPYBARA_SERVER_PORT"]
+    served_by host: "moiree", port: ENV["CAPYBARA_SERVER_PORT"]
+
+    driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400], options: {
+      browser: :remote,
+      url: "http://#{ENV["SELENIUM_HOST"]}:4444",
+    }
+  else
+    driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+  end
 
   def sign_in_as(user)
     visit(sign_in_url)
