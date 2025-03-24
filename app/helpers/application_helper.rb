60d7ce02193646e0b1307095b4a7f38783fde018
Mina Mikhail <mina@fightingtheboss.com>
2025-03-24 02:12:57 +0000

    Add open graph tags to editions views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,23 @@ module ApplicationHelper
   def description(content)
     content_for(:description) { "#{content} on MOIRÉE" }
   end
+
+  def open_graph(edition)
+    content_for(:open_graph, open_graph_tags(edition))
+  end
+
+  def open_graph_tags(edition)
+    tags = {
+      title: edition.code,
+      url: edition_url(edition),
+      image: overview_edition_share_url(edition),
+      description: content_for(:description),
+      site_name: "MOIRÉE",
+      locale: "en_US",
+    }
+
+    tags.map do |key, value|
+      tag(:meta, property: "og:#{key}", content: value)
+    end.join.html_safe
+  end
 end
