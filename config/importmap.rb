16dc9156e64bca44c02839fe2cfe75a6b76f6c54
Mina Mikhail <mina@fightingtheboss.com>
2025-04-05 20:15:18 +0000

    Add generated assets for ActionText

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,5 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@rails/request.js", to: "@rails--request.js.js" # @0.0.9
 pin "sortablejs" # @1.15.2
+pin "trix"
+pin "@rails/actiontext", to: "actiontext.esm.js"
