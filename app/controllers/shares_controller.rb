2edc7189d8587704d169a2f95d2193a33665d26c
Mina Mikhail <mina@fightingtheboss.com>
2025-05-01 08:54:20 +0000

    Remove references to vips (for now)

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -3,40 +3,38 @@
 class SharesController < ApplicationController
   before_action :set_edition
 
-  require "vips"
-
   def overview
     if stale?(@edition)
-      respond_to do |format|
-        format.svg
-        format.png do
-          svg = render_to_string("shares/overview", formats: :svg, layout: false, locals: { edition: @edition })
+      # respond_to do |format|
+      #   format.svg
+      #   format.png do
+      #     svg = render_to_string("shares/overview", formats: :svg, layout: false, locals: { edition: @edition })
 
-          vips_image = Vips::Image.svgload_buffer(svg, access: :sequential, dpi: 300)
+      #     vips_image = Vips::Image.svgload_buffer(svg, access: :sequential, dpi: 300)
 
-          logo = ImageProcessing::Vips
-            .source(Rails.root.join("app/assets/images/moiree-logo.png"))
-            .resize_to_limit(160, 160)
-            .call
+      #     logo = ImageProcessing::Vips
+      #       .source(Rails.root.join("app/assets/images/moiree-logo.png"))
+      #       .resize_to_limit(160, 160)
+      #       .call
 
-          summary = ImageProcessing::Vips
-            .source(vips_image)
-            .resize_to_fill(1100, 530)
-            .loader(page: 0, density: 300) # Adjust density for higher resolution
-            .convert("png")
-            .call
+      #     summary = ImageProcessing::Vips
+      #       .source(vips_image)
+      #       .resize_to_fill(1100, 530)
+      #       .loader(page: 0, density: 300) # Adjust density for higher resolution
+      #       .convert("png")
+      #       .call
 
-          image = ImageProcessing::Vips
-            .source(Rails.root.join("app/assets/images/moiree-bg-sm.png"))
-            .resize_to_fill(1200, 630)
-            .composite(summary, gravity: "north-west", offset: [50, 50])
-            .composite(logo, gravity: "north-west", offset: [900, 100])
-            .convert("png")
-            .call
+      #     image = ImageProcessing::Vips
+      #       .source(Rails.root.join("app/assets/images/moiree-bg-sm.png"))
+      #       .resize_to_fill(1200, 630)
+      #       .composite(summary, gravity: "north-west", offset: [50, 50])
+      #       .composite(logo, gravity: "north-west", offset: [900, 100])
+      #       .convert("png")
+      #       .call
 
-          send_data(File.read(image.path), type: "image/png", disposition: "inline")
-        end
-      end
+      #     send_data(File.read(image.path), type: "image/png", disposition: "inline")
+      #   end
+      # end
     end
   end
 
