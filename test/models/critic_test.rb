6add2a12694e61ebbe722b8bee9b380231415072
Mina Mikhail <mina@fightingtheboss.com>
2025-05-10 20:46:05 +0000

    Add tests for new methods on Critic

--- test/models/critic_test.rb
+++ test/models/critic_test.rb
@@ -69,4 +69,57 @@ class CriticTest < ActiveSupport::TestCase
 
     assert_not(critic.has_custom_publication?(edition: edition))
   end
+
+  test "#attending? returns true when critic is attending an edition" do
+    critic = critics(:base)
+    edition = editions(:base)
+
+    assert(critic.attending?(edition: edition))
+  end
+
+  test "#attending? returns false when critic is not attending an edition" do
+    critic = critics(:base)
+    non_attended_edition = festivals(:base).editions.create!(code: "TEST", year: Date.current.year, start_date: Date.current, end_date: Date.current + 7.days)
+
+    assert_not(critic.attending?(edition: non_attended_edition))
+  end
+
+  test "#editions.current returns editions happening now" do
+    critic = critics(:base)
+    current_edition = festivals(:base).editions.create!(
+      code: "CURRENT",
+      year: Date.current.year,
+      start_date: Date.current - 2.days,
+      end_date: Date.current + 5.days,
+    )
+    critic.editions << current_edition
+
+    assert_includes critic.editions.current, current_edition
+  end
+
+  test "#editions.past returns editions that have ended" do
+    critic = critics(:base)
+    past_edition = festivals(:base).editions.create!(
+      code: "PAST",
+      year: Date.current.year - 1,
+      start_date: Date.current - 30.days,
+      end_date: Date.current - 20.days,
+    )
+    critic.editions << past_edition
+
+    assert_includes critic.editions.past, past_edition
+  end
+
+  test "#editions.upcoming returns editions that haven't started" do
+    critic = critics(:base)
+    future_edition = festivals(:base).editions.create!(
+      code: "FUTURE",
+      year: Date.current.year + 1,
+      start_date: Date.current + 30.days,
+      end_date: Date.current + 40.days,
+    )
+    critic.editions << future_edition
+
+    assert_includes critic.editions.upcoming, future_edition
+  end
 end
