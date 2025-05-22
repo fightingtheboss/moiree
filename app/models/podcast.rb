de9500f07bb0876299cabb182cc4d744d9266409
Mina Mikhail <mina@fightingtheboss.com>
2025-05-22 04:36:42 +0000

    Add the Podcast::Edition model to the app

--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -6,6 +6,7 @@ class Podcast < ApplicationRecord
   friendly_id :slug_candidates, use: :slugged
 
   belongs_to :user
+  has_many :episodes, dependent: :destroy, class_name: "Podcast::Episode"
 
   validates :title, :slug, presence: true
 
