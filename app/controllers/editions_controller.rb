643b4411725862683b8d0b99b6682b78f6021424
Mina Mikhail <mina@fightingtheboss.com>
2025-07-29 20:42:25 +0000

    Reduce allocations on the Editions#show view

--- app/controllers/editions_controller.rb
+++ app/controllers/editions_controller.rb
@@ -6,7 +6,7 @@ class EditionsController < ApplicationController
   def show
     hidden_critics = params[:critics]
 
-    @edition = Edition.friendly.find(params[:id])
+    @edition = edition = Edition.friendly.find(params[:id])
     @selections = @edition.selections.includes(:category, :film, ratings: :critic).order("films.title")
 
     if only_show_rated?
@@ -14,10 +14,22 @@ class EditionsController < ApplicationController
     end
 
     @selections_by_category = @selections.group_by(&:category).sort_by { |category, _selections| category&.position }
+    @film_directors = @selections.map(&:film).uniq.index_with { |film| film.directors.join(", ") }
 
-    @critics = @edition.critics.sort_by(&:last_name).reject do |critic|
-      hidden_critics&.include?(critic.id.to_s)
-    end
+    @critics = @edition.critics
+      .includes(:attendances)
+      .order(last_name: :asc)
+      .reject { |critic| hidden_critics&.include?(critic.id.to_s) }
+      .each do |critic|
+        # Set the publication for the critic based on their attendance at the edition
+        # Do this dynamically to avoid N+1 queries
+        critic.define_singleton_method(:publication) do
+          attendances.find { |attendance| attendance.edition_id == edition.id }&.publication || super()
+        end
+
+        # TODO: Can just persist this in the database
+        critic.define_singleton_method(:country_emoji) { Country[country].emoji_flag }
+      end
 
     @ratings = @critics.each_with_object({}) do |critic, selection_hash|
       selection_hash[critic] = @selections.each_with_object({}) do |selection, rating_hash|
