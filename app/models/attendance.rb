e82f4387160efda53b27d594e2f29c2e293d442c
Mina Mikhail <mina@fightingtheboss.com>
2025-07-30 20:01:50 +0000

    Add caching to the editions#show view

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Attendance < ApplicationRecord
-  belongs_to :critic
+  belongs_to :critic, touch: true
   belongs_to :edition, touch: true
 
   validates :critic_id, uniqueness: { scope: :edition_id }
