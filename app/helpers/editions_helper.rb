9419e7421cdb0e6fc31c291f4013e67de9a58e6c
Mina Mikhail <mina@fightingtheboss.com>
2025-08-05 15:43:38 +0000

    Ensure that edition cache keys use  query params

--- app/helpers/editions_helper.rb
+++ app/helpers/editions_helper.rb
@@ -10,4 +10,10 @@ module EditionsHelper
       score
     end
   end
+
+  def params_cache_key
+    Digest::MD5.hexdigest(
+      params.to_unsafe_h.except(:controller, :action, :format, :locale).to_s,
+    )
+  end
 end
