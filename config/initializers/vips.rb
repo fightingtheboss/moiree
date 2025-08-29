d4db61a9a03ebdb6a2dfdccc7c697ee0b2a4fd26
Mina Mikhail <mina@fightingtheboss.com>
2025-08-29 17:33:33 +0000

    Require vips for in initializer for development

--- config/initializers/vips.rb
+++ config/initializers/vips.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "vips"
+
 Vips.cache_set_max(0) # Disable all caching to avoid memory issues
 Vips.cache_set_max_mem(0) # Disable memory caching to avoid memory issues
 Vips.cache_set_max_files(0) # Disable file caching to avoid memory issues
