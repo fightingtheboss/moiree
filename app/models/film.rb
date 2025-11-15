86dac73a80e7ed8fe77aad51ed139bf9dd41327f
Mina Mikhail <mina@fightingtheboss.com>
2025-11-15 20:35:43 +0000

    Add ability to initialize a Film with data from TMDB

--- app/models/film.rb
+++ app/models/film.rb
@@ -28,6 +28,25 @@ class Film < ApplicationRecord
 
   friendly_id :slug_candidates, use: :slugged
 
+  class << self
+    def from_tmdb(tmdb_id)
+      tmdb_movie = TMDB.find(tmdb_id)
+
+      new(
+        title: tmdb_movie.title,
+        original_title: tmdb_movie.original_title,
+        director: tmdb_movie.directors.join(", "),
+        country: tmdb_movie.countries.join(","),
+        year: tmdb_movie.release_date.to_date&.year,
+        summary: tmdb_movie.overview,
+        release_date: tmdb_movie.release_date,
+        poster_path: tmdb_movie.poster_path,
+        backdrop_path: tmdb_movie.backdrop_path,
+        tmdb_id: tmdb_movie.id,
+      )
+    end
+  end
+
   def cache_overall_average_rating
     update(overall_average_rating: ratings.average(:score).to_f)
   end
