cedff876f884c9415d1877451794e12b3ddbe91e
Mina Mikhail <mina@fightingtheboss.com>
2026-05-18 23:12:10 -0400

    Refactor display_rating helper to be more idiomatic

--- app/helpers/editions_helper.rb
+++ app/helpers/editions_helper.rb
@@ -1,15 +1,29 @@
 # frozen_string_literal: true
 
 module EditionsHelper
-  def display_rating(score, walked_out: false)
-    if walked_out
-      content_tag(:span, "🚪🚶", class: "tracking-[-0.2rem] whitespace-nowrap")
-    elsif score.zero?
+  def format_rating(rating)
+    if rating.walked_out?
+      walked_out_indicator
+    elsif rating.score.zero?
+      "💣"
+    elsif rating.score == 5.0
+      "🔥"
+    else
+      rating.score
+    end
+  end
+
+  def display_score(score)
+    if score.zero?
       "💣"
     elsif score == 5.0
       "🔥"
     else
       score
     end
+  end
+
+  def walked_out_indicator
+    content_tag(:span, "🚪🚶", class: "tracking-[-0.2rem] whitespace-nowrap")
   end
 end
--- test/helpers/editions_helper_test.rb
+++ test/helpers/editions_helper_test.rb
@@ -3,30 +3,60 @@
 require "test_helper"
 
 class EditionsHelperTest < ActionView::TestCase
-  test "#display_rating returns a span with the walked-out emoji for walked_out: true" do
-    result = display_rating(0, walked_out: true)
+  # --- format_rating ---
+
+  test "#format_rating returns the walked-out indicator for a walked-out rating" do
+    rating = ratings(:base).tap { |r| r.walked_out = true }
+    result = format_rating(rating)
 
     assert_includes(result, "🚪🚶")
     assert_includes(result, "tracking-[-0.2rem]")
     assert_includes(result, "whitespace-nowrap")
   end
 
-  test "#display_rating ignores score when walked_out: true" do
-    result = display_rating(5.0, walked_out: true)
+  test "#format_rating ignores score when walked out" do
+    rating = ratings(:base).tap { |r| r.walked_out = true }
+    result = format_rating(rating)
+
+    assert_not_includes(result.to_s, "🔥")
+  end
+
+  test "#format_rating returns bomb emoji for zero score" do
+    rating = ratings(:base).tap { |r| r.score = 0 }
+    assert_equal("💣", format_rating(rating))
+  end
+
+  test "#format_rating returns fire emoji for 5.0 score" do
+    rating = ratings(:base).tap { |r| r.score = 5.0 }
+    assert_equal("🔥", format_rating(rating))
+  end
+
+  test "#format_rating returns score for values between 0 and 5" do
+    rating = ratings(:base)
+    assert_equal(rating.score, format_rating(rating))
+  end
+
+  # --- display_score ---
+
+  test "#display_score returns bomb emoji for zero" do
+    assert_equal("💣", display_score(0.0))
+  end
+
+  test "#display_score returns fire emoji for 5.0" do
+    assert_equal("🔥", display_score(5.0))
+  end
+
+  test "#display_score returns score for other values" do
+    assert_equal(3.5, display_score(3.5))
+  end
+
+  # --- walked_out_indicator ---
+
+  test "#walked_out_indicator returns a span with the walked-out emoji and styling classes" do
+    result = walked_out_indicator
 
     assert_includes(result, "🚪🚶")
-    assert_not_includes(result, "🔥")
-  end
-
-  test "#display_rating returns bomb emoji for zero score" do
-    assert_equal("💣", display_rating(0.0))
-  end
-
-  test "#display_rating returns fire emoji for 5.0 score" do
-    assert_equal("🔥", display_rating(5.0))
-  end
-
-  test "#display_rating returns score for values between 0 and 5" do
-    assert_equal(3.5, display_rating(3.5))
+    assert_includes(result, "tracking-[-0.2rem]")
+    assert_includes(result, "whitespace-nowrap")
   end
 end
